db8f3c647b59ae5f4215d074d594c815b1ec9d29
Faizan Ahmad <ahmad13faizan@gmail.com>
2024-04-23 23:40:29 +0530

    third commit

--- src/main/java/NumberGuessServlet.java
+++ src/main/java/NumberGuessServlet.java
@@ -58,19 +58,24 @@ public class NumberGuessServlet extends HttpServlet {
                 out.println("<form action=\"NumberGuessServlet\" method=\"POST\">");
                 out.println("<input type=\"submit\" value=\"Play Again\">");
                 out.println("</form>");
-                out.println("</div>");
+                
 
                 session.removeAttribute("numberToGuess");
                 session.removeAttribute("attempts");
             } else if (attempts >= 10 ) {
+                out.println("<html><head><title>Number Guessing Game</title>");
+                out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">");
+                out.println("</head><body>");
                 out.println("<div class=\"container\">");
-                out.println("<p>\"Sorry, you have reached the maximum number of attempts. The correct number was " + numberToGuess + ".</p>");
+                out.println("<p style=\"color: red; font-size: 24px; text-align: center;\">Sorry, you have reached the maximum number of attempts. The correct number was " + numberToGuess + ".</p>");
                 out.println("<marquee><h2 style=\"font-size: 36px; color: blue; text-align: center;\">Game Over</h2></marquee>");
                 out.println("<form action=\"NumberGuessServlet\" method=\"POST\">");
-                out.println("<input type=\"submit\" value=\"Play Again\">");
+                out.println("<input type=\"submit\" value=\"Play Again\" style=\"background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 10px;\">");
                 out.println("</form>");
                 out.println("</div>");
+                out.println("</body></html>");
                 
+
                 session.removeAttribute("numberToGuess");
                 session.removeAttribute("attempts");
             } else if (userGuess < numberToGuess) {
